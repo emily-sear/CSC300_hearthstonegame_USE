c6c961c9dc904d0c9a2a6662c21717e28862d975
Emily <emily.sear@cuw.edu>
2021-04-13 11:21:41 -0500

    changed stuff

--- driver.cpp
+++ driver.cpp
@@ -1,118 +1,45 @@
 #include "Card.hpp"
 #include "URL.hpp"
+#include "json.hpp"
 #include <iostream>
 #include <string>
 
+using json = nlohmann::json;
 using namespace std;
 
 // all functions before implementation so they can be used before they are implemented
-string processJsonArray(string jsonString, int start);
-string processJsonObject(string jsonString, int start);
+
 
 int main(int argc, char** argv)
 {
-    //URL* u1 = new URL("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
-    //cout << u1->getContents() << endl;
+    URL* u1 = new URL("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
+    string jsonString = u1->getContents(); // this is a string of the URL above
+    json parsedJson = json::parse(jsonString);
 
-    string testJson = "[{fname:val1, lname: val2, sums:[1,2,3]},{fname:val3, lname:val4}]";
+    Card* theCards[parsedJson.size()];
 
-    processJsonObject(testJson, 0);
-    //cout << whatsReturned << endl; 
+
+    if(parsedJson.is_array())
+    {
+        for(int i = 0; i < parsedJson.size(); i++)
+        {
+            json currValue = parsedJson[i];
+            if(currValue.is_object()) // really don't need to ask but ya know
+            {
+                string name = currValue.value("name", "N/A"); // this gives us the individual cards
+                int attack = currValue.value("attack", -1);
+                int defense = currValue.value("health", -1);
+                int manaCost = currValue.value("cost", -1);
+                theCards[i] = new Card(name, manaCost, attack, defense);
+                theCards[i]->display();
+            }
+ 
+        }
+    } 
 
     return 0;
 }
 
-string processJsonArray(string jsonString, int start)
-{   
-
-    string temp = "";
-    int count = 1;
-
-    for(int i = start + 1; i < jsonString.length(); i++)
-    {   
-        if(jsonString.at(i) == '[')
-        {
-            string answer = processJsonArray(jsonString, i);
-            temp += answer;
-            i += answer.length() + 1;
-            count++;
-        }
-        else if(jsonString.at(i) == ']')
-        {
-            count--;
-            if(count == 0)
-            {
-                cout << temp << endl;
-                return temp;
-            }
-        }
-        else if(jsonString[i] == '{')
-        {
-            temp += '{';
-            string answer = processJsonObject(jsonString, i);
-            temp += answer;
-            i += answer.length() + 1;
-        }
-        if( i < jsonString.length())
-        {
-            temp = temp + jsonString[i];
-        }
-    }
-
-    //return "Something went wrong";
-
-}
-
-string processJsonObject(string jsonString, int start)
-{
-    if(jsonString[start] == '[')
-    {
-        return processJsonArray(jsonString, start);
-    }
-    else if(jsonString[start] == '{')
-    {
-        //must be a JSON Object 
-
-        string temp = "";
-        int countCurlyBraces = 1;
-        for(int i = start + 1; i < jsonString.length(); i++)
-        {   
-            
-            if(jsonString[i] == '{')
-            {   temp += '{';
-                string answer = processJsonObject(jsonString, i);
-                temp += answer;
-
-                //you force i to jump ahead 
-                i += answer.length()+1;
-                countCurlyBraces++;
-            }
-            else if(jsonString.at(i) == '}')
-            {
-                countCurlyBraces--;
-                if(countCurlyBraces == 0)
-                {
-                    cout << temp << endl;
-                    return temp; 
-                    
-                }
-            }
-            else if(jsonString.at(i) == '[')
-            {
-                temp += '[';
-                string answer = processJsonArray(jsonString, i);
-                temp += answer;
-                i += answer.length();
-            }
-
-            if(i < jsonString.length())
-            {
-                temp = temp + jsonString[i];
-            }
-        }
-    
-    }
-}
 
 
 
